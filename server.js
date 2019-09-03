7f0d72d86c0677a41c7b6b12097df679a49b8f6b
pdpsinghr <pradeeprathore.jecrc@gmail.com>
2019-09-03 10:12:53 +0530

    pushed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('xpress');
+const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname + "/dist/"));
